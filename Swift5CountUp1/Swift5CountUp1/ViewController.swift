c5f26a219d2cccbbe80782426ccdc663c1dee4bc
github user name <thetake1206@gmail.com>
2019-10-30 14:08:44 +0900

    [fix]カウントアップアプリを完了

--- Swift5CountUp1/Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/Swift5CountUp1/ViewController.swift
@@ -1,20 +1,50 @@
-//
-//  ViewController.swift
-//  Swift5CountUp1
-//
-//  Created by Yasuyuki Takeshita on 2019/10/30.
-//  Copyright © 2019 Yasuyuki Takeshita. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countLabel.text = "0"
+
     }
 
+    @IBAction func plus(_ sender: Any) {
+        // カウントアップ
+         count = count + 1
+        // ラベルに文字を反映したい（数字）
+        countLabel.text = String(count)
 
+
+        // 10以上になったら黄色に
+        if count >= 10 {
+                  changeTextColor()
+              }
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        // カウントアップ
+        count = count - 1
+
+        // ラベルに文字を反映したい（数字）
+        countLabel.text = String(count)
+
+        // 0以下になったら白色に
+        if count <= 0 {
+            resetTextColor()
+        }
+        
+    }
+    
+    // 新しくメソッドが作成された
+    func changeTextColor(){
+        countLabel.textColor = .yellow
+    }
+    
+    func resetTextColor(){
+        countLabel.textColor = .white
+    }
+    
 }
 
