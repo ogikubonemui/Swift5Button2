e2d58fec8a82e4acf10777c31be1ec6003530737
github user name <thetake1206@gmail.com>
2019-11-06 18:59:55 +0900

    [add]

---
+++ keyboard/keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func login(_ sender: Any) {
+        
+        
+    }
+    
+}
+
